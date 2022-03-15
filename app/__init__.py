6c4d2808e8b9600aead7559e979347d3dcd3a32e
tecnosam <ikabolo59@gmail.com>
2022-03-15 15:08:38 +0100

    Improved .env info

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import TEST_MODE
 
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
 
 flask_app = Flask(__name__)
 flask_app.secret_key = b'sample'
-flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(flask_app)
